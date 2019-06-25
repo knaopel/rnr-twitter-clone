6adeb0226cc3f2784f334a811dcfbb2771025a5d
Kurt Opel <kurt@kurtopel.com>
2019-06-25 15:01:46 -0400

    webpack and babel transpiling

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: './app/assets/javascripts/components/main.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
