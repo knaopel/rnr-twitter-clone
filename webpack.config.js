04595462d34a915f18fb926419adf2280ed17060
Kurt Opel <kurt@kurtopel.com>
2019-06-25 15:52:35 -0400

    Bundling with webpack and source maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,20 @@
+const webpack = require('webpack');
+
 module.exports = {
-  entry: './app/assets/javascripts/components/main.jsx',
+  entry: './app/assets/frontend/main.jsx',
+  devtool: false,
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[name].js.map'
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/app/assets/javascripts',
+    filename: 'bundle.js'
+  },
   module: {
     rules: [
       {
