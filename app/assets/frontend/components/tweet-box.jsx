4e78ae74cfc68d975adbef364166760e08909195
Kurt Opel <kurt@kurtopel.com>
2019-06-25 21:38:59 -0400

    mocks complete

--- app/assets/frontend/components/tweet-box.jsx
+++ app/assets/frontend/components/tweet-box.jsx
@@ -1,13 +1,21 @@
 export default class TweetBox extends React.Component {
+  sendTweet(event) {
+    event.preventDefault();
+    this.props.sendTweet(this.refs.tweetTextArea.value);
+    this.refs.tweetTextArea.value = '';
+  }
+
   render() {
     return (
-      <form>
-        <div className="input-field">
-          <textarea className="materialize-textarea" />
-          <label>What's happening?</label>
-          <button className="btn right">Tweet</button>
-        </div>
-      </form>
-    )
+      <div className="row">
+        <form onSubmit={this.sendTweet.bind(this)}>
+          <div className="input-field">
+            <textarea ref="tweetTextArea" className="materialize-textarea" />
+            <label>What's happening?</label>
+            <button type="submit" className="btn right">Tweet</button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,10 +1,32 @@
 import TweetBox from './components/tweet-box';
+import TweetList from './components/tweet-list';
+
+// const mockTweets = [
+//   { id: 1, name: 'Kurt Opel', body: 'my #firsttweet' },
+//   { id: 2, name: 'Kurt Opel', body: 'my #secondtweet' },
+//   { id: 3, name: 'Kurt Opel', body: 'my #thirdtweet' }
+// ];
+
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tweetsList: []
+    };
+  }
+  addTweet(tweetToAdd) {
+    let newTweetsList = this.state.tweetsList;
+    newTweetsList.unshift({ id: Date.now(), name: 'Guest', body: tweetToAdd })
+
+    this.setState({ tweetsList: newTweetsList });
+  }
+
   render() {
     return (
       <div className="container">
-        <TweetBox />
+        <TweetBox sendTweet={this.addTweet.bind(this)} />
+        <TweetList tweets={this.state.tweetsList} />
       </div>
     );
   }
